9fa49c2117e1a0a940c7eb9394c5b9c4ccbabc23
Joseph Senecal <jasenecal2@gmail.com>
2020-09-11 04:18:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ I want to use return for the first two and console.log in
 the next two. Last will need a conditional workflow management system*/
 //1) receives one argument and returns it in all caps1
 function shout (input) {
-  return input.toUpperCase;
+  return input.toUpperCase();
 }
 //2) receives one argument and returns it in all lowercase
 function whisper (input) {
-  return input.toLowerCase;
+  return input.toLowerCase();
 }
 //3) calls console.log() its one argument in all caps
 function logShout (input) {
-  console.log(input.toUpperCase);
+  console.log(input.toUpperCase());
 }
 //4) calls console.log() its one argument in all lowercase
 function logWhisper (input) {
-  console.log(input.toLowerCase);
+  console.log(input.toLowerCase());
 }
 //5) returns "I can't hear you!" if `string` is lowercase
 //6) returns "YES INDEED!" if `string` is uppercase
@@ -24,7 +24,7 @@ function sayHiToGrandma (input) {
   if (input === "I love you, Grandma.") {
     return "I love you, too."
   }
-  else if (input === input.toUpperCase) {
+  else if (input === input.toUpperCase()) {
     return "YES INDEED!"
   }
   else {
